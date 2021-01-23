d08857c77e941763072888bb1c5a3ee06af507af
changan wang <1660883200@qq.comgit config --global user.name changangit commit -m 1>
2021-01-23 16:52:00 +0800

    测试2

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+//配置路径
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views',
+            }
+        }
+    }
+}
